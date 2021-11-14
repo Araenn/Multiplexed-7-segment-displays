b32a053ca4c59b9e9d9812a4f5f22478d796ee89
Léa Yeromonahos <51425332+Araenn@users.noreply.github.com>
2021-11-14 17:06:49 +0100

    Update and rename exo1_4.ino.ino to M7SD.ino

--- M7SD.ino
+++ M7SD.ino
@@ -1,3 +1,8 @@
+//Two 74HC595 shift registers are used to select one of the 4 displays (outputs Qa to Qd of the U2 circuit on logic high level) 
+//and to switch on (low level) or off (high level) one of the segments (a to g) or the decimal point (h) (outputs Qa to Qh of the U3 circuit)
+//(high level) or the decimal point (h) (outputs Qa to Qh of circuit U3).
+//We will create two tables containing the numbers to be sent to U2 and U3, such as ://
+
 unsigned char table[] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90};
 unsigned char sel[] = {0xF1,0xF2,0xF4,0xF8};
 unsigned char numbers[] = {1,2,3,4,5,6,7,8,9};
@@ -36,6 +41,11 @@ void loop(){
      show(numbers[i], i);
   }
 }
+
+//The show() subroutine displays the 4 numbers contained in the following array:
+//unsigned char digit1[] = 1, 2, 3, 4;. 
+//For each digit, we set the latch to 0, we use the shiftOut command for table and salt ://
+
 void show(int num, int sh){
   digitalWrite(latch, 0);
   shiftOut(data, clk, MSBFIRST, table[num]);
